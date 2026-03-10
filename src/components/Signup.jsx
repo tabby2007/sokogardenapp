895e671ad3570e4b1892d7b6b5f8c200de79acf3
Duma_Tabby <tabithaondego9@gmail.com>
2026-03-10 14:17:44 -0700

    updates

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
+  //Initialize the hooks
+  const [username, setUsername]= useState("");
+  const[email, setEmail] = useState("");
+  const[password,setPassword] = useState("");
+  const[phone,setPhone] = useState("");
   return (
-    <div>
-        <h1>Welcome to the signup page</h1>
+    <div className='row justify-content-center mt-4'>
+        <div className="card col-md-6 shadow p-4">
+          <h1 className='text-primary'>Sign Up</h1>
+
+          <form>
+            <input type="text"
+            placeholder='Enter the username'
+            className='form-control'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required /> <br />
+
+            {/* {username} */}
+
+            <input type="email"
+            placeholder='Enter the email address'
+            className='form-control'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} /> <br />
+
+            {/* {email} */}
+
+            <input type="password"
+            placeholder='Enter the password'
+            className='form-control' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/> <br />
+
+            {/* {password} */}
+
+            <input type="tel"
+            placeholder='Enter the mobile phone number'
+            className='form-control'
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)} /> <br />
+
+            {/* {phone} */}
+
+            <input type="button" value="Signup" className='btn btn-primary' /> <br /><br />
+
+            Already have an account? <Link to={'/signin'}>signin</Link>
+          </form>
+        </div>
     </div>
   )
 }
