7b2f413f001c1828cc388957735f171e726c573f
Duma_Tabby <tabithaondego9@gmail.com>
2026-03-12 14:23:39 -0700

    new

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Signin = () => {
+
+  //Define the two hooks for capturing/storing the users input
+  const[email,setEmail]= useState("");
+  const[password,setPassword]= useState();
+
+  //Declare the three additional hooks
+  const[loading,setLoading]=useState("");
+  const[success,setSuccess]=useState("");
+  const[error,setError]=useState("");
+
+  //below we have the useNavigate hook to redirect us to another page on successful login
+  const navigate = useNavigate()
+
+  //below is the function to handle the sirgrin action
+  const handleSubmit = async (e)=>{
+    //prevent the site from reloading
+    e.preventDefault()
+
+    //update the loading hook with a message
+    setLoading("Please wait while we aunthenticate your account...")
+
+    try{
+      //create a formData object that will hold the email and the password
+      const formdata = new FormData()
+
+      //Insert/append the email and the password on the formData created
+      formdata.append("email",email)
+      formdata.append("password",password);
+
+      //interact with the axios for the response
+      const response= await axios.post("https://kbenkamotho.alwaysdata.net/api/signin",formdata)
+
+      //Set loading back to default
+      setLoading("");
+
+      //Check whether the user exists as part of your response from the API
+      if(response.data.user){
+        //if user is there, definetly he details entered during sign in are correct
+        // setSuccess("Login successful")
+        //if it is successful, let a person get redirected to another page
+        navigate("/")
+
+      }
+      else{
+        //user is not found, that means the credentials entered on the form are incorrect
+        setError("Login failed. Please try again...")
+      }
+    }
+    catch(error){
+      //set loading back to default
+      setLoading("")
+
+      //Update the error hook with message
+      setError("Oops,something went wrong.Try again...")
+    }
+  }
+
   return (
-    <div>
-        <h1>Welcome to the signin page</h1>
+    <div className='row justify-content-center mt-4'>
+        <div className='col-md-6 card shadow p-4'>
+          <h1 className='text-primary'>Sign In</h1>
+          <h5 className="text-info">{loading}</h5>
+          <h3 className="text-success">{success}</h3>
+          <h4 className="text-danger">{error}</h4>
+
+          <form onSubmit={handleSubmit}>
+            <input type="email"
+            placeholder='Enter the email address'
+            className='form-control'
+            required 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/> <br />
+            {/* {email} */}
+
+            <input type="password"
+            placeholder='Enter the password here'
+            className='form-control'
+            required
+            value={password}
+            onChange={(e) =>setPassword(e.target.value)} /> <br />
+            {/* {password} */}
+
+            <input type="submit"
+            value= "Sign In"
+            className='btn btn-primary'/>
+          </form>
+        </div>
     </div>
   )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -7,12 +8,71 @@ const Signup = () => {
   const[email, setEmail] = useState("");
   const[password,setPassword] = useState("");
   const[phone,setPhone] = useState("");
+
+  //Define the three states an application will move to
+  const[loading,setLoading]=useState("");
+  const[success,setSuccess]=useState("");
+  const[error,setError]=useState("");
+
+  //Below is a function that will handle the submit action
+  const handleSubmit =async(e)=>{
+    //Below we prevent our site from reloading
+    e.preventDefault()
+
+    //Update our loading hook with a message that will be displayed to the users who are trying to register
+    setLoading("Please wait as registration continues...")
+
+    try{
+      //create a form data object that will enable you to capture the four details entered on the form
+      const formdata= new FormData();
+
+      //Insert the four details(username, email, password, phone) in terms of key-value pairs
+      formdata.append("username", username);
+      formdata.append("email",email);
+      formdata.append("password",password);
+      formdata.append("phone",phone);
+
+      //By use of axios, we can access the method post
+      const response =await axios.post("https://tabbyondego.alwaysdata.net/api/signup",formdata)
+
+      //set back the loading to default
+      setLoading("");
+
+      //Just incase everything goes on weel, update the success hook with a message
+      setSuccess(response.data.Message)
+
+      //Clear your hooks
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+
+       setTimeout(() => {
+    setSuccess("");
+  }, 5000);
+
+    }
+    catch(error){
+      //Set the loading back to default
+      setLoading("");
+
+      //Update the error hook with the message given back from the response
+      setError(error.message)
+    }
+  }
+
+
   return (
     <div className='row justify-content-center mt-4'>
         <div className="card col-md-6 shadow p-4">
           <h1 className='text-primary'>Sign Up</h1>
 
-          <form>
+          <h5 className="text-warning"> {loading}</h5>
+          <h3 className="text-success">{success}</h3>
+          <h4 className="text-danger">{error}</h4>
+
+          <form onSubmit={handleSubmit}>
+
             <input type="text"
             placeholder='Enter the username'
             className='form-control'
@@ -46,7 +106,7 @@ const Signup = () => {
 
             {/* {phone} */}
 
-            <input type="button" value="Signup" className='btn btn-primary' /> <br /><br />
+            <input type="submit" value="Signup" className='btn btn-primary' /> <br /><br />
 
             Already have an account? <Link to={'/signin'}>signin</Link>
           </form>
